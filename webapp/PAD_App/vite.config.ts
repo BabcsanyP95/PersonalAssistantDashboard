25e220675152c36a7cc637b4bc83b6e391b0ad10
BabcsanyP95 <petibabcsany@gmail.com>
2026-07-09 11:33:26 +0200

    Add Docker development environment

--- webapp/PAD_App/vite.config.ts
+++ webapp/PAD_App/vite.config.ts
@@ -1,18 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    vueDevTools(),
+    vue()
   ],
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+      '@': path.resolve(__dirname, './src')
+    }
   },
+
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  }
 })
